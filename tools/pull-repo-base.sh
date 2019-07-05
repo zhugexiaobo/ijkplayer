e5de4ce4174f855469679827bfd0a8da2b496598
zhugexiaobo <zhugexiaobo@gmail.com>
2019-07-05 15:01:11 +0800

    feat: init speed up

--- tools/pull-repo-base.sh
+++ tools/pull-repo-base.sh
@@ -7,9 +7,9 @@ LOCAL_WORKSPACE=$2
 if [ -z $REMOTE_REPO -o -z $LOCAL_WORKSPACE ]; then
     echo "invalid call pull-repo.sh '$REMOTE_REPO' '$LOCAL_WORKSPACE'"
 elif [ ! -d $LOCAL_WORKSPACE ]; then
-    git clone $REMOTE_REPO $LOCAL_WORKSPACE
+    proxychains4 git clone $REMOTE_REPO $LOCAL_WORKSPACE
 else
     cd $LOCAL_WORKSPACE
-    git fetch --all --tags
+    proxychains4 git fetch --all --tags
     cd -
 fi
--- tools/pull-repo-ref.sh
+++ tools/pull-repo-ref.sh
@@ -7,11 +7,11 @@ REF_REPO=$3
 if [ -z $1 -o -z $2 -o -z $3 ]; then
     echo "invalid call pull-repo.sh '$1' '$2' '$3'"
 elif [ ! -d $LOCAL_WORKSPACE ]; then
-    git clone --reference $REF_REPO $REMOTE_REPO $LOCAL_WORKSPACE
+    proxychains4 git clone --reference $REF_REPO $REMOTE_REPO $LOCAL_WORKSPACE
     cd $LOCAL_WORKSPACE
     git repack -a
 else
     cd $LOCAL_WORKSPACE
-    git fetch --all --tags
+    proxychains4 git fetch --all --tags
     cd -
 fi
